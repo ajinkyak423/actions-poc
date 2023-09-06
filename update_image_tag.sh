42a193d469f917d1c7adecc940306b4b1bd568d7
ajinkyak423 <ajinkyakumbhar423@gmail.com>
2023-09-06 23:31:21 +0530

    dsd

--- update_image_tag.sh
+++ update_image_tag.sh
@@ -5,6 +5,6 @@ image_name="summerwind/actions-runner"
 
 new_tag="$1"  # The newTag value is passed as the first argument
 
-sed -i "s/\( name: $image_name\n *newTag: \)./\1$new_tag/" "$file_path"
+sed -i "s/\( name: summerwind/actions-runner\n *newTag: \)./\1$1/" "kustomization.yml"
 
-echo "Image tag for $image_name updated to $new_tag"
+echo "Image tag for summerwind/actions-runner updated to $1"
