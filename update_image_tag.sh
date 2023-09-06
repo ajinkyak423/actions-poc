8b72c5cef95f631aaccabb32b3f7582950aa1662
ajinkyak423 <ajinkyakumbhar423@gmail.com>
2023-09-06 23:42:54 +0530

    d

--- update_image_tag.sh
+++ update_image_tag.sh
@@ -5,6 +5,6 @@ image_name="summerwind/actions-runner"
 
 new_tag="$1"  # The newTag value is passed as the first argument
 
-sed -i "s/ name: summerwind/actions-runner\n *newTag: .*/ name: summerwind/actions-runner\n  newTag: $new_tag/" "$file_path"
+sed -i "s| name: summerwind/actions-runner\n *newTag: .*| name: summerwind/actions-runner\n  newTag: $new_tag|" "$file_path"
 
 echo "Image tag for summerwind/actions-runner updated to $new_tag"
