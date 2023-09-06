1d7cdf9dc7897504863cbe1d6d7cd804c8c7f89e
ajinkyak423 <ajinkyakumbhar423@gmail.com>
2023-09-06 23:52:13 +0530

    f

--- update_image_tag.sh
+++ update_image_tag.sh
@@ -12,7 +12,7 @@ else
   # Perform the update and commit
   sed -i "s| name: summerwind/actions-runner\n *newTag: .*| name: summerwind/actions-runner\n  newTag: $new_tag|" "$file_path"
   echo "Image tag for summerwind/actions-runner updated to $new_tag"
-  
+
   git config --local user.email "action@github.com"
   git config --local user.name "GitHub Action"
   git checkout -b feature/update-file  # Create a new branch for the changes
