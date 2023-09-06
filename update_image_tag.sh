22ea244b32f5ece94f081cabb35aa024d5b8d9d2
ajinkyak423 <ajinkyakumbhar423@gmail.com>
2023-09-06 23:45:25 +0530

    ads

--- update_image_tag.sh
+++ update_image_tag.sh
@@ -5,6 +5,17 @@ image_name="summerwind/actions-runner"
 
 new_tag="$1"  # The newTag value is passed as the first argument
 
-sed -i "s| name: summerwind/actions-runner\n *newTag: .*| name: summerwind/actions-runner\n  newTag: $new_tag|" "$file_path"
-
-echo "Image tag for summerwind/actions-runner updated to $new_tag"
+# Check if there are changes to commit
+if git diff-index --quiet HEAD --; then
+  echo "No changes to commit."
+else
+  # Perform the update and commit
+  sed -i "s| name: summerwind/actions-runner\n *newTag: .*| name: summerwind/actions-runner\n  newTag: $new_tag|" "$file_path"
+  echo "Image tag for summerwind/actions-runner updated to $new_tag"
+  
+  git config --local user.email "action@github.com"
+  git config --local user.name "GitHub Action"
+  git checkout -b feature/update-file  # Create a new branch for the changes
+  git add .
+  git commit -m "Update file via GitHub Actions"
+fi
