0b101f3301840c1d659a192326fcca5e6e29d5ee
ajinkyak423 <ajinkyakumbhar423@gmail.com>
2023-09-08 00:47:32 +0530

    6.3

--- update_image_tag.py
+++ update_image_tag.py
@@ -1,22 +1,23 @@
-import yaml
+import re
 
-# Define the file path of your Kustomization file
+# Define the path to your Kustomization YAML file
 kustomization_file_path = 'kustomization.yml'
 
 # Define the newTag value you want to set
-new_tag = 'v2.305.0-ubuntu-20.04'
+new_tag_value = 'v2.305.0-ubuntu-20.04'
 
-# Load the Kustomization file
-with open(kustomization_file_path, 'r') as file:
-    kustomization_data = yaml.safe_load(file)
+# Read the content of the Kustomization file
+with open(kustomization_file_path, 'r') as kustomization_file:
+    file_content = kustomization_file.read()
 
-# Update the newTag value for the specified image
-for image in kustomization_data.get('images', []):
-    if image.get('name') == 'summerwind/actions-runner':
-        image['newTag'] = new_tag
+# Define a regular expression pattern to find the image entry
+pattern = r"^- name: summerwind/actions-runner[\s\S]*?newTag: .*$"
 
-# Write the modified Kustomization data back to the file
-with open(kustomization_file_path, 'w') as file:
-    yaml.dump(kustomization_data, file, default_flow_style=False)
+# Replace the newTag value in the image entry
+new_content = re.sub(pattern, f"- name: summerwind/actions-runner\n    newTag: {new_tag_value}", file_content, flags=re.MULTILINE)
 
-print(f"Updated newTag for 'summerwind/actions-runner' to '{new_tag}'")
+# Write the updated content back to the file
+with open(kustomization_file_path, 'w') as kustomization_file:
+    kustomization_file.write(new_content)
+
+print(f"Updated newTag for 'summerwind/actions-runner' to '{new_tag_value}'.")
