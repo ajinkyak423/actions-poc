0a1eb3eb12985fc00032c02ab8ef2e4bca81efe3
ajinkyak423 <ajinkyakumbhar423@gmail.com>
2023-09-07 20:38:13 +0530

    1

--- update_image_tag.sh
+++ update_image_tag.sh
@@ -10,17 +10,17 @@ if git diff-index --quiet HEAD --; then
   echo "No changes to commit."
 else
   # Perform the update only if the tag has changed
-  current_tag=$(grep -Po 'name: summerwind/actions-runner\n *newTag: \K.*' "$file_path")
+  current_tag=$(grep -oP "name: $image_name\n\s+newTag: \K\S+" "$file_path")
   if [ "$current_tag" != "$new_tag" ]; then
-    sed -i "s| name: summerwind/actions-runner\n *newTag: .*| name: summerwind/actions-runner\n  newTag: $new_tag|" "$file_path"
-    echo "Image tag for summerwind/actions-runner updated to $new_tag"
+    sed -i "s/\(name: $image_name\n\s*newTag: \).*/\1$new_tag/" "$file_path"
+    echo "Image tag for $image_name updated to $new_tag"
 
     git config --local user.email "action@github.com"
     git config --local user.name "GitHub Action"
     git checkout -b feature/update-file  # Create a new branch for the changes
     git add .
-    git commit -m "Update file via GitHub Actions"
+    git commit -m "Update $image_name tag via GitHub Actions"
   else
-    echo "No changes needed. Current tag is already $new_tag."
+    echo "No changes needed. Current tag for $image_name is already $new_tag."
   fi
 fi
