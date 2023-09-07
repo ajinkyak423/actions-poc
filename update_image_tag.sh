a9c8b2895dd80828e8695f5af8ffa70c5b0cb656
ajinkyak423 <ajinkyakumbhar423@gmail.com>
2023-09-07 20:43:41 +0530

    2.2

--- update_image_tag.sh
+++ update_image_tag.sh
@@ -4,4 +4,4 @@
 NEW_IMAGE_TAG="v2.305.0-ubuntu-20.04"
 
 # Update the image tag in the Kustomization file
-sed -i "s|name: summerwind/actions-runner.*|name: summerwind/actions-runner\n  newTag: $NEW_IMAGE_TAG|" kustomization.yaml
+sed -i "s|name: summerwind/actions-runner.*|name: summerwind/actions-runner\n  newTag: $NEW_IMAGE_TAG|" kustomization.yml
