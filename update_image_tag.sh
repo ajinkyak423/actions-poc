56a17e8948c655bcdee39ca0b56d6ea90900fb56
ajinkyak423 <ajinkyakumbhar423@gmail.com>
2023-09-07 21:17:36 +0530

    4.4

--- update_image_tag.sh
+++ update_image_tag.sh
@@ -4,11 +4,10 @@ file_path="kustomization.yml"
 image_name="summerwind/actions-runner"
 new_tag="v2.305.0-ubuntu-20.04" # The newTag value is passed as the first argument
 
-echo "Image name: $image_name"
-echo "New tag: $new_tag"
+# Escape special characters in image_name and new_tag
+image_name_escaped=$(sed 's/[[\.*^$/]/\\&/g' <<< "$image_name")
+new_tag_escaped=$(sed 's/[\/&]/\\&/g' <<< "$new_tag")
 
-sed -n "/\(name: $image_name\)/,/\(name: .*\)/p" "$file_path"
-
-sed -i "s|\(name: $image_name\n\s*newTag: \).*|\1$new_tag|" "$file_path"
+sed -i "s|\(name: $image_name_escaped\n\s*newTag: \).*|\1$new_tag_escaped|" "$file_path"
 
 echo "Image tag for $image_name updated to $new_tag"
