3ecd0a30a52efdf3ec845c3ba4e53bcb3bc12b34
ajinkyak423 <ajinkyakumbhar423@gmail.com>
2023-09-08 01:06:51 +0530

    6.7

--- update_image_tag.py
+++ update_image_tag.py
@@ -1,10 +1,9 @@
+
 import re
+import os
 
 # Define the path to your Kustomization YAML file
 kustomization_file_path = 'kustomization.yml'
-
-# Define the newTag value you want to set
-new_tag_value = 'v2.305.0-ubuntu-20.04'
 
 # Read the content of the Kustomization file
 with open(kustomization_file_path, 'r') as kustomization_file:
@@ -12,6 +11,9 @@ with open(kustomization_file_path, 'r') as kustomization_file:
 
 # Define a regular expression pattern to find the image entry
 pattern = r"^- name: summerwind/actions-runner[\s\S]*?newTag: .*$"
+
+# Extract the newTag value from the environment variable
+new_tag_value = os.environ.get('latest_version_previous_major')
 
 # Replace the newTag value in the image entry with proper indentation
 new_content = re.sub(pattern, f"- name: summerwind/actions-runner\n  newTag: {new_tag_value}", file_content, flags=re.MULTILINE)
