2018192e53494211ca5bd67e549d578ce5db31a5
ajinkyak423 <ajinkyakumbhar423@gmail.com>
2023-09-25 15:35:36 +0530

    as

--- temp.sh
+++ temp.sh
@@ -2,19 +2,11 @@
 
 release_metadata=$(curl -s https://api.github.com/repos/actions/runner/releases/latest)
 latest_release_date=$(date -d "$(echo "$release_metadata" | jq -r '.published_at')" '+%s')
-
-latest_release_date_formatted=$(date -d "$latest_release_date" '+%Y-%m-%d')
-echo "latest_release_date_formatted: $latest_release_date_formatted"
-
 echo "latest_release_date: $latest_release_date"
-
-
 
 current_date=$(date '+%s')
 echo "current_date: $current_date"
 
-current_date_formatted=$(date -d "$current_date" '+%Y-%m-%d')
-echo "current_date_formatted: $current_date_formatted"
 
 date_diff=$(( (current_date - latest_release_date) / 86400 ))  # Calculate the difference in days
 
