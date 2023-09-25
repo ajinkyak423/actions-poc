a8be669e4ea9e73561cbcefd1f662d5904e4a455
ajinkyak423 <ajinkyakumbhar423@gmail.com>
2023-09-25 15:43:15 +0530

    AS

--- temp.sh
+++ temp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 release_metadata=$(curl -s https://api.github.com/repos/actions/runner/releases/latest)
-latest_release_date=$(date -d "$(echo "$release_metadata" | jq -r '.published_at')" '+%s')
+latest_release_date=$(date -d "$(echo "$release_metadata" | jq -r '.published_at')")
 latest_release_date_formatted=$(date -d "@$latest_release_date" '+%Y-%m-%d')
 echo "latest_release_date (Y-M-D): $latest_release_date_formatted"
 
@@ -9,11 +9,11 @@ current_date=$(date '+%s')
 current_date_formatted=$(date -d "@$current_date" '+%Y-%m-%d')
 echo "current_date (Y-M-D): $current_date_formatted"
 
-date_diff=$(( (current_date - latest_release_date) / 86400 ))  # Calculate the difference in days
+date_diff=$(( (current_date - $(date -d "$latest_release_date" '+%s') ) / 86400 ))  # Calculate the difference in days
 
 echo "date_diff: $date_diff"
 
-if [ "$date_diff" -ge 0 ]; then
+if [ "$date_diff" -ge 30 ]; then
   echo "The latest version is $date_diff days old compared to the current date."
 else
   echo "The latest version is from the future."
